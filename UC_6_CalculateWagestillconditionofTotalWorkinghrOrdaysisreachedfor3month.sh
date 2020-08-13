5d667f7ab781adc8abc9662d48db7a17f5a59db7
anuja-garud <anujagarud24@gmail.com>
2020-08-13 21:19:11 +0530

    Added total working hr or day is reached for 3 month

---
+++ UC_6_CalculateWagestillconditionofTotalWorkinghrOrdaysisreachedfor3month.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+ratePerHour=20
+numberOfWorkingDay=20
+maxHour=10
+maxHour=100
+
+totalEmpHour=0
+totalWorkingDays=0
+while [[ $totalEmpHour -le $maxHour && $totalWorkingDays -le $numberOfWorkingDay ]]
+do
+((totalWorkingDays++))
+empCheck=$(( RANDOM%3 ));
+case $empCheck in
+        $isPartTime) echo "Emp part time salary"
+        empHour=4
+        ;;
+        $isFullTime)  echo "Emp full time salary"
+        empHour=8
+        ;;
+        *) echo "Emp is not exist"
+        empHour=0
+        ;;
+esac
+totalEmpHour=$(( $totalEmpHour+$empHour ))
+totalSalary=$(( $totalEmpHour * $ratePerHour ))
+done
