76b8fb58ae7a6087c3fa5d285dabd303b6cbc2f4
anuja-garud <anujagarud24@gmail.com>
2020-08-13 23:26:40 +0530

    Store the Day and Daily wage along with total wage

---
+++ UC_9_StoretheDayandDailyWagealongwiththeTotalWage.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+#Constatnt
+IS_PART_TIME=1
+IS_FULL_TIME=2
+RATE_PER_HOUR=125
+NUMBER_OF_WORKING_DAY=2
+MAX_HOURS=10
+declare -A dailyWages
+#Variable
+totalEmpHour=0
+totalWorkingDays=0
+
+function getDailyWages(){
+        local empHrs=$1
+        empSalary=$(( $empHrs*$RATE_PER_HOUR ))
+        echo $empSalary
+}
+function getEmpHours(){
+        case $empCheck in
+                $IS_PART_TIME)
+                empHour=4
+                ;;
+                $IS_FULL_TIME)
+                empHour=8
+                ;;
+                *)
+                empHour=0
+                ;;
+        esac
+                echo $empHour
+}
+
+while [[ $totalEmpHour -le $MAX_HOURS && $totalWorkingDays -le $NUMBER_OF_WORKING_DAY ]]
+do
+((totalWorkingDays++))
+empCheck=$(( RANDOM%3 ));
+
+empHour="$( getEmpHours $empCheck)"
+totalEmpHour=$(( $totalEmpHour + $empHour ))
+dailyWages[totalWorkingDays]="$( getDailyWages $empHour )"
+totalSalary="$( getDailyWages $totalEmpHour)"
+done
+echo ${dailyWages[@]}
+echo ${!dailyWages[@]}
