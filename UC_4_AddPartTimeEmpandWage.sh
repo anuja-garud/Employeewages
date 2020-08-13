ac99421f13b515015d94a270fa2e25c674320cd8
anuja-garud <anujagarud24@gmail.com>
2020-08-13 20:51:55 +0530

    Add Part time Emp & wage

---
+++ UC_4_AddPartTimeEmpandWage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+isPartTime=1
+isFullTime=2
+empRatePerHrs=20
+
+empCheck=$(( RANDOM%3 ))
+
+case $empCheck in
+        $isPartTime)
+                empHrs=4 ;;
+
+        $isFullTime)
+                empHrs=8 ;;
+
+        *)
+                empHrs=0 ;;
+esac
+sal=$(( $empHrs * $empRatePerHrs ))
